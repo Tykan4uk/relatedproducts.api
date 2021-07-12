9474625f6865eb55f7412fc0c2cef13842afa71a
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-12 17:44:28 +0300

    Adding RelatedProductEntity.cs and RelatedProductsDbContext.cs

---
+++ RelatedProductsApi/src/RelatedProductsApi/Data/RelatedProductEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelatedProductsApi.Data
+{
+    public class RelatedProductEntity
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ RelatedProductsApi/src/RelatedProductsApi/Data/RelatedProductsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RelatedProductsApi.Data
+{
+    public class RelatedProductsDbContext : DbContext
+    {
+        public RelatedProductsDbContext(DbContextOptions<RelatedProductsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RelatedProductEntity> RelatedProducts { get; set; } = null!;
+    }
+}
