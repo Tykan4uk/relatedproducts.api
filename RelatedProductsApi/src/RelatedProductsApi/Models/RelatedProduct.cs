2e64a46772aaba1e703d91a8c849afc710b6f14b
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-13 10:08:02 +0300

    Add class RelatedProduct

---
+++ RelatedProductsApi/src/RelatedProductsApi/Models/RelatedProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelatedProductsApi.Models
+{
+    public class RelatedProduct
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
