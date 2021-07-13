edb64018fb849eff42ad20430eb8d2305815f374
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-13 18:39:10 +0300

    Adding MappingProfile

---
+++ RelatedProductsApi/src/RelatedProductsApi/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RelatedProductsApi.Data;
+using RelatedProductsApi.Models;
+
+namespace RelatedProductsApi
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RelatedProduct, RelatedProductEntity>().ReverseMap();
+        }
+    }
+}
