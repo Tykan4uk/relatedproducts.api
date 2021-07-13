8529a51a5e7153bbe4029217b569408619173fbd
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-13 12:06:12 +0300

    Adding ManageController and RelatedProductBffContorller

---
+++ RelatedProductsApi/src/RelatedProductsApi/Controllers/RelatedProductBffController.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RelatedProductsApi.Configurations;
+using RelatedProductsApi.Models.Requests;
+using RelatedProductsApi.Services.Abstractions;
+
+namespace RelatedProductsApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    public class RelatedProductBffController : ControllerBase
+    {
+        private readonly ILogger<RelatedProductBffController> _logger;
+        private readonly IRelatedProductService _relatedProductService;
+        private readonly Config _config;
+
+        public RelatedProductBffController(
+            ILogger<RelatedProductBffController> logger,
+            IOptions<Config> config,
+            IRelatedProductService relatedProductService)
+        {
+            _logger = logger;
+            _config = config.Value;
+            _relatedProductService = relatedProductService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetByPage(GetByPageRequest getByPageRequest)
+        {
+            var result = await _relatedProductService.GetByPageAsync(getByPageRequest.Page);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetById(GetByIdRequest getByIdRequest)
+        {
+            var result = await _relatedProductService.GetByIdAsync(getByIdRequest.Id);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AddRequest addRequest)
+        {
+            var result = await _relatedProductService.AddAsync(addRequest.RelatedProduct);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Put([FromBody] UpdateRequest updateRequest)
+        {
+            var result = await _relatedProductService.UpdateAsync(updateRequest.RelatedProduct);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromBody] DeleteRequest deleteRequest)
+        {
+            var result = await _relatedProductService.DeleteAsync(deleteRequest.Id);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetPageCounter()
+        {
+            var result = await _relatedProductService.GetPageCounterAsync();
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+    }
+}
