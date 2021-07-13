efb040dfbd562130aec747cbed2b473980cb1676
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-13 11:16:39 +0300

    Adding IRalatedProductServices and RelatedProductService

---
+++ RelatedProductsApi/src/RelatedProductsApi/Services/Abstractions/IRelatedProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using RelatedProductsApi.Models;
+using RelatedProductsApi.Models.Responses;
+
+namespace RelatedProductsApi.Services.Abstractions
+{
+    public interface IRelatedProductService
+    {
+        Task<GetByPageResponse> GetByPageAsync(int page);
+        Task<GetByIdResponse> GetByIdAsync(Guid guid);
+        Task<AddResponse> AddAsync(RelatedProduct relatedProduct);
+        Task<DeleteResponse> DeleteAsync(Guid guid);
+        Task<UpdateResponse> UpdateAsync(RelatedProduct relatedProduct);
+        Task<GetPageCounterResponse> GetPageCounterAsync();
+    }
+}
---
+++ RelatedProductsApi/src/RelatedProductsApi/Services/RelatedProductService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using RelatedProductsApi.Data;
+using RelatedProductsApi.DataProviders.Abstractions;
+using RelatedProductsApi.Models;
+using RelatedProductsApi.Models.Responses;
+using RelatedProductsApi.Services.Abstractions;
+
+namespace RelatedProductsApi.Services
+{
+    public class RelatedProductService : IRelatedProductService
+    {
+        private readonly IRelatedProductProvider _relatedProductProvider;
+        private readonly MapperConfiguration _mapperConfiguration;
+        private readonly IMapper _mapper;
+
+        public RelatedProductService(IRelatedProductProvider relatedProductProvider)
+        {
+            _relatedProductProvider = relatedProductProvider;
+            _mapperConfiguration = new MapperConfiguration(cfg => cfg.CreateMap<RelatedProduct, RelatedProductEntity>());
+            _mapper = new Mapper(_mapperConfiguration);
+        }
+
+        public async Task<GetByPageResponse> GetByPageAsync(int page)
+        {
+            var result = await _relatedProductProvider.GetByPageAsync(page);
+
+            var relatedProducts = _mapper.Map<List<RelatedProduct>>(result);
+
+            return new GetByPageResponse() { RelatedProducts = relatedProducts };
+        }
+
+        public async Task<GetByIdResponse> GetByIdAsync(Guid guid)
+        {
+            var result = await _relatedProductProvider.GetByIdAsync(guid);
+
+            var relatedProduct = _mapper.Map<RelatedProduct>(result);
+
+            return new GetByIdResponse() { RelatedProduct = relatedProduct };
+        }
+
+        public async Task<AddResponse> AddAsync(RelatedProduct relatedProduct)
+        {
+            var relatedProductEntity = _mapper.Map<RelatedProductEntity>(relatedProduct);
+
+            var result = await _relatedProductProvider.AddAsync(relatedProductEntity);
+
+            var relatedProductOut = _mapper.Map<RelatedProduct>(result);
+
+            return new AddResponse() { RelatedProduct = relatedProductOut };
+        }
+
+        public async Task<DeleteResponse> DeleteAsync(Guid guid)
+        {
+            var result = await _relatedProductProvider.DeleteAsync(guid);
+
+            return new DeleteResponse() { IsDeleted = result };
+        }
+
+        public async Task<UpdateResponse> UpdateAsync(RelatedProduct relatedProduct)
+        {
+            var relatedProductEntity = _mapper.Map<RelatedProductEntity>(relatedProduct);
+
+            var result = await _relatedProductProvider.UpdateAsync(relatedProductEntity);
+
+            return new UpdateResponse() { IsUpdated = result };
+        }
+
+        public async Task<GetPageCounterResponse> GetPageCounterAsync()
+        {
+            var result = await _relatedProductProvider.GetPageCounterAsync();
+
+            return new GetPageCounterResponse() { PageCounter = result };
+        }
+    }
+}
