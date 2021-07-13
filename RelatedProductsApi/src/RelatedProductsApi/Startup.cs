56e74c2b07cd431cb8b0dfb1820271c68185c126
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-13 11:22:28 +0300

    Startup: adding services\providers, configuration and connection string for db

--- RelatedProductsApi/src/RelatedProductsApi/Startup.cs
+++ RelatedProductsApi/src/RelatedProductsApi/Startup.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RelatedProductsApi.Configurations;
+using RelatedProductsApi.Data;
+using RelatedProductsApi.DataProviders;
+using RelatedProductsApi.DataProviders.Abstractions;
+using RelatedProductsApi.Services;
+using RelatedProductsApi.Services.Abstractions;
 
 namespace RelatedProductsApi
 {
@@ -18,10 +18,14 @@ namespace RelatedProductsApi
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            AppConfiguration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration AppConfiguration { get; set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -30,6 +34,12 @@ namespace RelatedProductsApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RelatedProductsApi", Version = "v1" });
             });
+            services.Configure<Config>(AppConfiguration);
+            var connectionString = AppConfiguration["RelatedProductsApi:ConnectionString"];
+            services.AddDbContext<RelatedProductsDbContext>(
+                opts => opts.UseNpgsql(connectionString));
+            services.AddTransient<IRelatedProductProvider, RelatedProductProvider>();
+            services.AddTransient<IRelatedProductService, RelatedProductService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
