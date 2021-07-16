2e815f2c219a5f191da3603df672da91e229b9d5
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-16 14:36:44 +0300

    MappingProfile: adding new map: GetByPageResponse=>PagingDataResult,AddRequest=>RelatedProductEntity

--- RelatedProductsApi/src/RelatedProductsApi/Data/Mapping/MappingProfile.cs
+++ RelatedProductsApi/src/RelatedProductsApi/Data/Mapping/MappingProfile.cs
@@ -1,5 +1,8 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using RelatedProductsApi.Models;
+using RelatedProductsApi.Models.Requests;
+using RelatedProductsApi.Models.Responses;
 
 namespace RelatedProductsApi.Data.Mapping
 {
@@ -8,6 +11,10 @@ namespace RelatedProductsApi.Data.Mapping
         public MappingProfile()
         {
             CreateMap<RelatedProduct, RelatedProductEntity>().ReverseMap();
+            CreateMap<GetByPageResponse, PagingDataResult>()
+                .ForMember(dest => dest.RelatedProductsEntity, opt => opt.MapFrom(src => src.RelatedProducts))
+                .ReverseMap();
+            CreateMap<AddRequest, RelatedProductEntity>().BeforeMap((src, dest) => dest.Id = Guid.NewGuid().ToString());
         }
     }
 }
