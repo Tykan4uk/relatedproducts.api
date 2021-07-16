2fbcbecdc0f0be91fa37f65f0d0433b1fada9409
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-16 14:37:13 +0300

    Adding PagingDataResult

---
+++ RelatedProductsApi/src/RelatedProductsApi/Data/PagingDataResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RelatedProductsApi.Data
+{
+    public class PagingDataResult
+    {
+        public IEnumerable<RelatedProductEntity> RelatedProductsEntity { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
