4ce6eaa26501ba5f631facfd18c4412d5c7f11bd
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-16 14:46:21 +0300

    IRalatedProductProvider: deleting GetPageCounterAsync, change id type Guid=>string, change return type GetByPageAsync IEnumerable<RelatedProductEntity>=>PagingDataResult

--- RelatedProductsApi/src/RelatedProductsApi/DataProviders/Abstractions/IRelatedProductProvider.cs
+++ RelatedProductsApi/src/RelatedProductsApi/DataProviders/Abstractions/IRelatedProductProvider.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using RelatedProductsApi.Data;
 
 namespace RelatedProductsApi.DataProviders.Abstractions
 {
     public interface IRelatedProductProvider
     {
-        Task<IEnumerable<RelatedProductEntity>> GetByPageAsync(int page);
-        Task<RelatedProductEntity> GetByIdAsync(Guid id);
+        Task<PagingDataResult> GetByPageAsync(int page, int pageSize);
+        Task<RelatedProductEntity> GetByIdAsync(string id);
         Task<RelatedProductEntity> AddAsync(RelatedProductEntity relatedProductEntity);
-        Task<bool> DeleteAsync(Guid id);
+        Task<bool> DeleteAsync(string id);
         Task<bool> UpdateAsync(RelatedProductEntity relatedProductEntity);
-        Task<int> GetPageCounterAsync();
     }
 }
