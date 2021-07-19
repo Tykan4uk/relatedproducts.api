3697edc620f55b7347a2c24bc65ed0cd29dc3450
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 20:29:21 +0300

    RelatedProductService: adding use wrap-method to all methods

--- RelatedProductsApi/src/RelatedProductsApi/Services/RelatedProductService.cs
+++ RelatedProductsApi/src/RelatedProductsApi/Services/RelatedProductService.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RelatedProductsApi.Data;
 using RelatedProductsApi.DataProviders.Abstractions;
 using RelatedProductsApi.Models;
@@ -10,14 +10,16 @@ using RelatedProductsApi.Services.Abstractions;
 
 namespace RelatedProductsApi.Services
 {
-    public class RelatedProductService : IRelatedProductService
+    public class RelatedProductService : BaseDataService, IRelatedProductService
     {
         private readonly IRelatedProductProvider _relatedProductProvider;
         private readonly IMapper _mapper;
 
         public RelatedProductService(
+            IDbContextFactory<RelatedProductsDbContext> factory,
             IRelatedProductProvider relatedProductProvider,
             IMapper mapper)
+            : base(factory)
         {
             _relatedProductProvider = relatedProductProvider;
             _mapper = mapper;
@@ -25,45 +27,60 @@ namespace RelatedProductsApi.Services
 
         public async Task<GetByPageResponse> GetByPageAsync(GetByPageRequest getByPageRequest)
         {
-            var result = await _relatedProductProvider.GetByPageAsync(getByPageRequest.Page, getByPageRequest.PageSize);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _relatedProductProvider.GetByPageAsync(getByPageRequest.Page, getByPageRequest.PageSize);
 
-            return _mapper.Map<GetByPageResponse>(result);
+                return _mapper.Map<GetByPageResponse>(result);
+            });
         }
 
         public async Task<GetByIdResponse> GetByIdAsync(GetByIdRequest getByIdRequest)
         {
-            var result = await _relatedProductProvider.GetByIdAsync(getByIdRequest.Id);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _relatedProductProvider.GetByIdAsync(getByIdRequest.Id);
 
-            var relatedProduct = _mapper.Map<RelatedProduct>(result);
+                var relatedProduct = _mapper.Map<RelatedProduct>(result);
 
-            return new GetByIdResponse() { RelatedProduct = relatedProduct };
+                return new GetByIdResponse() { RelatedProduct = relatedProduct };
+            });
         }
 
         public async Task<AddResponse> AddAsync(AddRequest addRequest)
         {
-            var relatedProductEntity = _mapper.Map<RelatedProductEntity>(addRequest);
+            return await ExecuteSafe(async () =>
+            {
+                var relatedProductEntity = _mapper.Map<RelatedProductEntity>(addRequest);
 
-            var result = await _relatedProductProvider.AddAsync(relatedProductEntity);
+                var result = await _relatedProductProvider.AddAsync(relatedProductEntity);
 
-            var relatedProductOut = _mapper.Map<RelatedProduct>(result);
+                var relatedProductOut = _mapper.Map<RelatedProduct>(result);
 
-            return new AddResponse() { RelatedProduct = relatedProductOut };
+                return new AddResponse() { RelatedProduct = relatedProductOut };
+            });
         }
 
         public async Task<DeleteResponse> DeleteAsync(DeleteRequest deleteRequest)
         {
-            var result = await _relatedProductProvider.DeleteAsync(deleteRequest.Id);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _relatedProductProvider.DeleteAsync(deleteRequest.Id);
 
-            return new DeleteResponse() { IsDeleted = result };
+                return new DeleteResponse() { IsDeleted = result };
+            });
         }
 
         public async Task<UpdateResponse> UpdateAsync(RelatedProduct relatedProduct)
         {
-            var relatedProductEntity = _mapper.Map<RelatedProductEntity>(relatedProduct);
+            return await ExecuteSafe(async () =>
+            {
+                var relatedProductEntity = _mapper.Map<RelatedProductEntity>(relatedProduct);
 
-            var result = await _relatedProductProvider.UpdateAsync(relatedProductEntity);
+                var result = await _relatedProductProvider.UpdateAsync(relatedProductEntity);
 
-            return new UpdateResponse() { IsUpdated = result };
+                return new UpdateResponse() { IsUpdated = result };
+            });
         }
     }
 }
