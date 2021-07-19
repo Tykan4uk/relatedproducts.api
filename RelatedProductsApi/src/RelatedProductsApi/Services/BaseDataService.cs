3d5bc5a48c2b8c4ede630e2c5f168d5e4ba188f5
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 20:27:07 +0300

    Adding class-wrap BaseDataService

---
+++ RelatedProductsApi/src/RelatedProductsApi/Services/BaseDataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RelatedProductsApi.Data;
+
+namespace RelatedProductsApi.Services
+{
+    public class BaseDataService
+    {
+        private readonly RelatedProductsDbContext _relatedProductsDbContext;
+
+        public BaseDataService(IDbContextFactory<RelatedProductsDbContext> dbContextFactory)
+        {
+            _relatedProductsDbContext = dbContextFactory.CreateDbContext();
+        }
+
+        protected async Task<T> ExecuteSafe<T>(Func<Task<T>> action)
+        {
+            using (var transaction = _relatedProductsDbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var result = await action();
+                    transaction.Commit();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
