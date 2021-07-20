eed71e70d1e6bebdd557fb30a495e96e6b81d944
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 23:36:23 +0300

    GetByPageRequest: adding property SortedType

--- RelatedProductsApi/src/RelatedProductsApi/Models/Requests/GetByPageRequest.cs
+++ RelatedProductsApi/src/RelatedProductsApi/Models/Requests/GetByPageRequest.cs
@@ -1,8 +1,11 @@
-﻿namespace RelatedProductsApi.Models.Requests
+﻿using RelatedProductsApi.Common.Enums;
+
+namespace RelatedProductsApi.Models.Requests
 {
     public class GetByPageRequest
     {
         public int Page { get; set; }
         public int PageSize { get; set; }
+        public SortedTypeEnum SortedType { get; set; }
     }
 }
