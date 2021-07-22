62b9b9e76d54dad89acf3b7b48522807e50a6061
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-22 10:43:53 +0300

    Program: adding UseUrls 5003

--- RelatedProductsApi/services/RelatedProductsApi.Service/Program.cs
+++ RelatedProductsApi/services/RelatedProductsApi.Service/Program.cs
@@ -21,6 +21,7 @@ namespace RelatedProductsApi.Service
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:5003");
                 });
     }
 }
