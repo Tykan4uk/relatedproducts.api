ba8b66cfdfff4ee13034ad0f4d0f12b8c7ac9a49
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-24 20:44:43 +0300

    Adding BusinessException

---
+++ RelatedProductsApi/src/RelatedProductsApi/Common/Exceptions/BusinessException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RelatedProductsApi.Common.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message)
+        {
+            Message = message;
+        }
+
+        public override string Message { get; }
+    }
+}
