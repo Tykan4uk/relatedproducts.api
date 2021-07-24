aa91651ebf694a64463a1ebb86dbae676703793d
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-24 22:20:46 +0300

    Adding MockService

---
+++ RelatedProductsApi/tests/RelatedProductsApi.UnitTests/Mocks/MockService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using RelatedProductsApi.Data;
+using RelatedProductsApi.Services;
+using RelatedProductsApi.Services.Abstractions;
+
+namespace RelatedProductsApi.UnitTests.Mocks
+{
+    public class MockService : BaseDataService<RelatedProductsDbContext>
+    {
+        public MockService(
+            IDbContextWrapper<RelatedProductsDbContext> dbContextWrapper,
+            ILogger<MockService> logger)
+            : base(dbContextWrapper, logger)
+        {
+        }
+
+        public async Task RunWithException()
+        {
+            await ExecuteSafe<bool>(() =>
+            {
+                throw new Exception();
+            });
+        }
+
+        public async Task RunWithoutException()
+        {
+            await ExecuteSafe<bool>(() =>
+            {
+                return Task.FromResult(true);
+            });
+        }
+    }
+}
