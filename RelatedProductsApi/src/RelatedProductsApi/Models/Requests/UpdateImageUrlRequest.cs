2756503b599755c2ca422f982cc336bd31ae6635
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 11:38:18 +0300

    Adding UpdateImageUrlRequest

---
+++ RelatedProductsApi/src/RelatedProductsApi/Models/Requests/UpdateImageUrlRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RelatedProductsApi.Models.Requests
+{
+    public class UpdateImageUrlRequest
+    {
+        public string Id { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
