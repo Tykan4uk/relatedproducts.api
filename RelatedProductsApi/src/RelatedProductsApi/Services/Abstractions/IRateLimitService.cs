ebe20e3ec28eae12d32c9415a0b34c2d7072c459
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-26 19:17:32 +0300

    Adding IRateLimitService and RateLimitService

---
+++ RelatedProductsApi/src/RelatedProductsApi/Services/Abstractions/IRateLimitService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using RelatedProductsApi.Models.Responses;
+
+namespace RelatedProductsApi.Services.Abstractions
+{
+    public interface IRateLimitService
+    {
+        Task<CheckRateLimitResponse> CheckRateLimit(string name);
+    }
+}
---
+++ RelatedProductsApi/src/RelatedProductsApi/Services/RateLimitService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using RelatedProductsApi.Models.Responses;
+using RelatedProductsApi.Services.Abstractions;
+
+namespace RelatedProductsApi.Services
+{
+    public class RateLimitService : IRateLimitService
+    {
+        private readonly IHttpClientService _httpClientService;
+
+        public RateLimitService(IHttpClientService httpClientService)
+        {
+            _httpClientService = httpClientService;
+        }
+
+        public async Task<CheckRateLimitResponse> CheckRateLimit(string name)
+        {
+            string url = $"http://192.168.1.120:5004/api/v1/Manage/CheckRateLimit?name={name}";
+            var response = await _httpClientService.SendAsync<CheckRateLimitResponse>(url, HttpMethod.Get);
+            return response;
+        }
+    }
+}
